ca276f45e888ae17cc253f52664589c2aa09e5d7
Marie <marietta.sv88@gmail.com>
2022-08-19 14:28:33 +0300

    text

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -25,19 +25,24 @@ export default function MainPage({ setDiscrFlat, discrFlat, authState }) {
       <ul className="d-flex flex-wrap justify-content-around">
         {discrFlat && discrFlat.map((el) => (
           <li className="list-group-item">
-            <div className="card" style={{ width: '18rem', height: '30rem' }}>
+            <div
+              className="card"
+              style={{
+                width: '18rem', height: '30rem', borderWidth: '0.5', borderColor: 'darkgrey', boxShadow: '11px 14px 9px 1px  silver'
+              }}
+            >
               <img src={el.img} className="img-thumbnail" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">
                   Цена:
                   {' '}
                   {el.price}
-                  ₽
+                  ₽ /мес
                 </h5>
                 <p className="card-text">{el.descriptions}</p>
                 <p className="card-text">{el.coordinate}</p>
                 <div className="d-grid gap-2 d-md-flex justify-content-md-right">
-                  <Link to={`/houses/edit/${el.id}`} className="btn btn-secondary">Подробнее</Link>
+                  <Link to={`/houses/edit/${el.id}`} className="btn btn-secondary" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Подробнее</Link>
                   <button style={{ disabled: !buttonState }} onClick={(e) => addHandler(e, el.id)} to={`/favorite/add/${el.id}`} type="submit" className="btn btn-success">Добавить в избранное</button>
                 </div>
               </div>
